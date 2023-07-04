a5f101fa773c066bd458a6e07ea221ad281f3d2b
Christian Memije <7193975+christianmemije@users.noreply.github.com>
2023-07-03 20:16:25 -0700

    feat(): adds project descriptions

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,16 +11,18 @@ export default function Home() {
   const play = () => {
     if (audio) {
       audio.stop();
+      setAudio(undefined);
+    } else {
+      const sound = new Howl({
+        src: ['vibe.mp3'],
+        autoplay: true,
+        loop: true,
+        volume: 0.25,
+        preload: true,
+      });
+      sound.seek(6);
+      setAudio(sound);
     }
-    const sound = new Howl({
-      src: ['vibe.mp3'],
-      autoplay: true,
-      loop: true,
-      volume: 0.25,
-      preload: true,
-    });
-    sound.seek(6);
-    setAudio(sound);
   };
 
   useEffect(() => {
@@ -55,31 +57,41 @@ export default function Home() {
             <a href="https://mindfulgram.app" target="_blank">
               Mindfulgram
             </a>
+            <br />A web extension to use Instagram in a mindful way.
           </li>
           <li>
             <a href="https://thegiftapp.org" target="_blank">
               The Gift App
             </a>
+            <br />
+            An app to increase self worth.
           </li>
           <li>
-            <a href="https://dev-kube.trapnstudio.net" target="_blank">
+            <a href="https://vibecue.app" target="_blank">
               Vibecue
             </a>
+            <br />A platform that will democratize the music that is played.
           </li>
           <li>
             <a href="https://sortspotifyplaylists.com" target="_blank">
               Playsorter
             </a>
+            <br />A web app that sorts Spotify playlists based on various
+            features.
           </li>
           <li>
             <a href="https://drilllatino.net" target="_blank">
-              Drill Latino Net
+              Drill Latino
             </a>
+            <br />A multi-platform Latin Drill playlist.
           </li>
           <li>
             <a href="https://trapcorridosboard.com" target="_blank">
               Trap y Corridos Soundboard
             </a>
+            <br />A web app that allows users to play and share catchphrases
+            that are popular among the music genres of Latin Trap and Corridos
+            via a soundboard-like interface.
           </li>
         </ul>
 
@@ -89,11 +101,15 @@ export default function Home() {
             <a href="https://memije.io" target="_blank">
               Christian
             </a>
+            <br />
+            Founder
           </li>
           <li>
-            <a href="carlosmemije.github.io/carlosmemije" target="_blank">
+            <a href="https://www.linkedin.com/in/carlosmemije" target="_blank">
               Carlos
             </a>
+            <br />
+            Software engineer
           </li>
         </ul>
 
